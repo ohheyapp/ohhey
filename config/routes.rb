c22ee27a966105fcf7416ff3a8415658c5d4fd1b
hsbacot <hsbacot@gmail.com>
2014-12-06 16:26:03 -0500

    new message implemented

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   resources :messages
 
-  get 'messages/new/:receiver_id' => 'messages#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
