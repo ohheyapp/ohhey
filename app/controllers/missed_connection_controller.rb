96fbbd02d2bdc7acb712e3af0774bfff7c1a5bed
Brooks Swinnerton <bswinnerton@gmail.com>
2014-12-06 12:59:23 -0500

    MissedConnection controller

--- app/controllers/missed_connection_controller.rb
+++ app/controllers/missed_connection_controller.rb
@@ -1,7 +1,5 @@
 class MissedConnectionController < ApplicationController
   def index
-    @missed_connections = [
-      OpenStruct.new(title: 'You had a handlebar mustache', body: 'It was great', latitude: 40.707437, longitude: -73.925189)
-    ]
+    @missed_connections = MissedConnection.all
   end
 end
