877413d60217f2a21ec92848a876276a855cb5b0
Brooks Swinnerton <bswinnerton@gmail.com>
2014-12-09 17:17:19 -0500

    Frontend improvements

--- app/controllers/missed_connection_verifications_controller.rb
+++ app/controllers/missed_connection_verifications_controller.rb
@@ -9,7 +9,7 @@ class MissedConnectionVerificationsController < ApplicationController
     @missed_connection = MissedConnection.find(params[:missed_connection_id])
 
     if verification_params[:text] == @missed_connection.answer
-      redirect_to new_missed_connection_message_path(@missed_connection)
+      redirect_to new_missed_connection_message_path(@missed_connection), notice: "That really was you!"
     else
       flash.now[:alert] = "Sorry, it's not love."
       render :new
