7f395fdcacdc1a76fca45699e936d2b88c002243
Brooks Swinnerton <bswinnerton@gmail.com>
2015-01-31 17:22:27 -0500

    Adding RSpec

--- test/helpers/missed_connection_verifications_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MissedConnectionVerificationsHelperTest < ActionView::TestCase
-end
--- test/models/missed_connection_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MissedConnectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
