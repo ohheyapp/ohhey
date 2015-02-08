6d0943604b406682fd2bbbf0ad35e55e8901746c
Jennifer Spriggs <jenniferlspriggs@gmail.com>
2015-02-07 20:03:42 -0500

    working on landing page

--- app/assets/javascripts/missed_connection.js
+++ app/assets/javascripts/missed_connection.js
@@ -10,7 +10,15 @@ var App = {
     var missedConnectionParam = window.location.pathname.match(/missed_connections\/(\d*)/);
     var mapOptions = {
       zoom: 12,
-      center: new google.maps.LatLng(40.711815, -73.968449)
+      panControl: false,
+      streetViewControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.RIGHT_TOP,
+      },
+      center: new google.maps.LatLng(40.711815, -73.968449),
+      styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
     };
 
     App.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
